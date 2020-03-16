9914f5212c07a11c01b6b725a6d08c4670a0409b
unknown <hyerinoh1108@gmail.com>
2020-03-16 17:54:27 +0900

    removed node_modules

--- node_modules/react-data-grid/lib/common/cells/headerCells/FilterableHeaderCell.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-import { FilterRendererProps } from '../../types';
-export default function FilterableHeaderCell<R>({ column, onChange }: FilterRendererProps<R>): JSX.Element;
--- node_modules/react-data-grid/lib/formatters/SimpleCellFormatter.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export function SimpleCellFormatter(_a) {
-    var value = _a.value;
-    return React.createElement("div", { title: String(value) }, value);
-}
-//# sourceMappingURL=SimpleCellFormatter.js.map
