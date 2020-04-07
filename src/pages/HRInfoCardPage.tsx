bc98a32fdb33934b4e76254a5a3edfe0e26e633a
unknown <hyerinoh1108@gmail.com>
2020-04-07 09:57:00 +0900

    install xlsx

--- src/pages/HRInfoCardPage.tsx
+++ src/pages/HRInfoCardPage.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import EmployeeInfoLists from '../components/HRInfoCard';
+// import EmployeeInfoLists from '../components/HRInfoCard';
+import {AgGridReact} from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 export const HRInfoCardPage: React.FC = () => {
+    const columnDefs = [
+        {headerName : 'Name', field : 'name'},
+        {headerName : 'Title', field : 'title'}
+    ];
+    const rowData = [
+        {name : '1', title : '2'},
+        {name : '3', title : '4'},
+    ];
+
     return (
-        <div>
-            <EmployeeInfoLists/>
-        </div>
+        <div className="w-full">
+            <div className="ag-react-container" style={ {height: '200px', width: '600px'} }>
+            <AgGridReact
+                columnDefs={columnDefs}
+                rowData={rowData}
+                rowSelection="single">
+            </AgGridReact>
+        </div></div>
     )
 }
